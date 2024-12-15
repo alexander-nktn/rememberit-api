b03b7c20a45f770003a69a61e81f92981a175fd2
AlexanderNikitin <alexander.nktn.86@gmail.com>
2024-12-15 18:11:14 -0500

    fix: migrations;

--- src/main/java/rememberit/Rememberit.java
+++ src/main/java/rememberit/Rememberit.java
@@ -2,8 +2,10 @@ package rememberit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "rememberit")
 public class Rememberit {
 
 	public static void main(String[] args) {
